b875eb5706d014e3585f66ba6b24754d8f9bf7a2
Achaka <oen-feedback@yandex.ru>
2021-11-04 14:50:08 +0300

    coding challenge 02.2 done

--- app/src/main/java/com/example/android/twoactivities/MainActivity.java
+++ app/src/main/java/com/example/android/twoactivities/MainActivity.java
@@ -1,39 +1,74 @@
 package com.example.android.twoactivities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
-    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
     public static final int TEXT_REQUEST = 1;
-    public static final String EXTRA_MESSAGE = "com.example.android.twoactivities.extra.MESSAGE";
-    private EditText mMessageEditText;
-    private TextView mReplyHeadTextView;
-    private TextView mReplyTextView;
+    private int position = 0;
+    private final ArrayList<TextView> items = new ArrayList();
+    private static final String STRING_ARRAY_KEY = "com.example.android.twoactivities.STRINGS";
+    private static final String POSITION_KEY = "com.example.android.twoactivities.Position";
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        ArrayList<String> strings = new ArrayList<>();
+        for (TextView item : items) {
+            strings.add(item.getText().toString());
+        }
+        outState.putInt(POSITION_KEY, position);
+        outState.putStringArrayList(STRING_ARRAY_KEY, strings);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        ArrayList<String> stringsArray = savedInstanceState.getStringArrayList(STRING_ARRAY_KEY);
+        position = savedInstanceState.getInt(POSITION_KEY);
+        for (int i = 0; i < stringsArray.size(); i++) {
+            items.get(i).setText(stringsArray.get(i));
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Log the start of the onCreate() method.
-        Log.d(LOG_TAG, "-------");
-        Log.d(LOG_TAG, "onCreate");
 
-        mMessageEditText = findViewById(R.id.editText_main);
-        mReplyHeadTextView = findViewById(R.id.text_header_reply);
-        mReplyTextView = findViewById(R.id.text_message_reply);
+        TextView textItem1 = findViewById(R.id.text_item1);
+        TextView textItem2 = findViewById(R.id.text_item2);
+        TextView textItem3 = findViewById(R.id.text_item3);
+        TextView textItem4 = findViewById(R.id.text_item4);
+        TextView textItem5 = findViewById(R.id.text_item5);
+        TextView textItem6 = findViewById(R.id.text_item6);
+        TextView textItem7 = findViewById(R.id.text_item7);
+        TextView textItem8 = findViewById(R.id.text_item8);
+        TextView textItem9 = findViewById(R.id.text_item9);
+        TextView textItem10 = findViewById(R.id.text_item10);
+
+        items.add(textItem1);
+        items.add(textItem2);
+        items.add(textItem3);
+        items.add(textItem4);
+        items.add(textItem5);
+        items.add(textItem6);
+        items.add(textItem7);
+        items.add(textItem8);
+        items.add(textItem9);
+        items.add(textItem10);
     }
 
     public void launchSecondActivity(View view) {
         Intent intent = new Intent(this, SecondActivity.class);
-        String message = mMessageEditText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
         startActivityForResult(intent, TEXT_REQUEST);
     }
 
@@ -45,46 +80,12 @@ public class MainActivity extends AppCompatActivity {
             if (resultCode == RESULT_OK) {
                 String reply =
                         data.getStringExtra(SecondActivity.EXTRA_REPLY);
-                mReplyHeadTextView.setVisibility(View.VISIBLE);
-                mReplyTextView.setText(reply);
-                mReplyTextView.setVisibility(View.VISIBLE);
+                if (position < items.size()) {
+                    items.get(position).setText(reply);
+                    position++;
+                }
             }
         }
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d(LOG_TAG, "onStart");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d(LOG_TAG, "onPause");
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.d(LOG_TAG, "onRestart");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d(LOG_TAG, "onResume");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d(LOG_TAG, "onStop");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d(LOG_TAG, "onDestroy");
-    }
 }
--- app/src/main/java/com/example/android/twoactivities/SecondActivity.java
+++ app/src/main/java/com/example/android/twoactivities/SecondActivity.java
@@ -4,34 +4,65 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.view.MenuItem;
+import android.widget.Button;
+
+import java.util.ArrayList;
+
 
 public class SecondActivity extends AppCompatActivity {
+
+    private final ArrayList<Button> buttons = new ArrayList();
     public static final String EXTRA_REPLY = "com.example.android.twoactivities.extra.REPLY";
-    private EditText mReply;
-    private static final String LOG_TAG = SecondActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        Intent intent = getIntent();
-        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-        TextView textView = findViewById(R.id.text_message);
-        textView.setText(message);
-        mReply = findViewById(R.id.editText_second);
+
+        Button button1 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
+        Button button3 = findViewById(R.id.button3);
+        Button button4 = findViewById(R.id.button4);
+        Button button5 = findViewById(R.id.button5);
+        Button button6 = findViewById(R.id.button6);
+        Button button7 = findViewById(R.id.button7);
+        Button button8 = findViewById(R.id.button8);
+        Button button9 = findViewById(R.id.button9);
+        Button button10 = findViewById(R.id.button10);
+
+        buttons.add(button1);
+        buttons.add(button2);
+        buttons.add(button3);
+        buttons.add(button4);
+        buttons.add(button5);
+        buttons.add(button6);
+        buttons.add(button7);
+        buttons.add(button8);
+        buttons.add(button9);
+        buttons.add(button10);
+
+        for (Button button : buttons) {
+            button.setOnClickListener(view -> returnReply(button));
+        }
     }
 
-    public void returnReply(View view) {
-        String reply = mReply.getText().toString();
+
+    public void returnReply(Button button) {
+        String reply = button.getText().toString();
         Intent replyIntent = new Intent();
         replyIntent.putExtra(EXTRA_REPLY, reply);
         setResult(RESULT_OK, replyIntent);
-        Log.d(LOG_TAG, "End SecondActivity");
         finish();
     }
 
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return (super.onOptionsItemSelected(item));
+    }
 }
